c2f593061c8170af137a4660fb47b95ebfcdfa6c
Gabriel Sanzone <gabriel.sanzone@fcamara.com.br>
2019-11-12 20:12:48 -0300

    Contrução do serviço de DNS
    foi tbm introduzido o HTTP do webserver para teste

---
+++ BikeStop/DNS.ino
@@ -0,0 +1,68 @@
+#include <ESP8266mDNS.h>
+
+String host = "bikestop";
+
+WiFiServer server(80);
+void InicializaDNS(){
+  if (!MDNS.begin(host)) {
+   //DEBUGGING("Erro ao iniciar o servico mDNS!");
+    while (1) {
+      delay(1000);
+    }
+  }
+
+
+  //DEBUGGING("O servico mDNS foi iniciado com sucesso!");
+  MDNS.addService("http", "tcp", 80);
+}
+
+void loopNDS(){
+  MDNS.update();
+
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  Serial.println("");
+  Serial.println("New client");
+
+  // Wait for data from client to become available
+  while (client.connected() && !client.available()) {
+    delay(1);
+  }
+
+  // Read the first line of HTTP request
+  String req = client.readStringUntil('\r');
+
+  // First line of HTTP request looks like "GET /path HTTP/1.1"
+  // Retrieve the "/path" part by finding the spaces
+  int addr_start = req.indexOf(' ');
+  int addr_end = req.indexOf(' ', addr_start + 1);
+  if (addr_start == -1 || addr_end == -1) {
+    Serial.print("Invalid request: ");
+    Serial.println(req);
+    return;
+  }
+  req = req.substring(addr_start + 1, addr_end);
+  Serial.print("Request: ");
+  Serial.println(req);
+  client.flush();
+
+  String s;
+  if (req == "/") {
+    IPAddress ip = WiFi.localIP();
+    String ipStr = paginaHTML();
+    Serial.println("Sending 200");
+  } else {
+    s = "HTTP/1.1 404 Not Found\r\n\r\n";
+    Serial.println("Sending 404");
+  }
+  client.print(s);
+
+  Serial.println("Done with client");
+}
+
+String paginaHTML(){
+  String html = "";
+}
