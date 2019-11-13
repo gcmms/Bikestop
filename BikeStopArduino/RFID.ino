71775ac76a7fa7bb2982098952c920b791bfa009
Gabriel Sanzone <gabriel.sanzone@fcamara.com.br>
2019-11-12 21:18:00 -0300

    Iniialização de variaveis e pinos para o RFID

--- BikeStopArduino/RFID.ino
+++ BikeStopArduino/RFID.ino
@@ -1,1 +1,16 @@
+#include <MFRC522.h> //biblioteca responsável pela comunicação com o módulo RFID-RC522
+#include <SPI.h> //biblioteca para comunicação do barramento SPI
 
+#define SS_PIN    21
+#define RST_PIN   22
+ 
+#define SIZE_BUFFER     18
+#define MAX_SIZE_BLOCK  16
+ 
+
+MFRC522::MIFARE_Key key;
+
+MFRC522::StatusCode status;
+
+// Definicoes pino modulo RC522
+MFRC522 mfrc522(SS_PIN, RST_PIN); 
